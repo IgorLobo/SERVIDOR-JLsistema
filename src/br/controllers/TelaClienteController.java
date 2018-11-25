7eb1c178482446cbb37b584239806424d4bd0a54
unknown <hygordebarross@gmail.com>
2018-11-25 21:34:57 -0200

    Listar tela cliente

--- src/br/controllers/TelaClienteController.java
+++ src/br/controllers/TelaClienteController.java
@@ -1,13 +1,16 @@
 package br.controllers;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javax.swing.JOptionPane;
 
 import br.model.Cliente;
+import br.persistencia.ClienteDAO;
 import br.util.Janela;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -31,29 +34,30 @@ public class TelaClienteController implements Initializable {
 
 	@FXML
 	private Button btn_excluir;
-	
+
 	@FXML
-    private Button btn_observacao;
+	private Button btn_observacao;
 
-    @FXML
-    private TableView<Cliente> tableView_cliente;
+	@FXML
+	private TableView<Cliente> tableView_cliente;
 
-    @FXML
-    private TableColumn<Cliente, Integer> tc_ID;
+	@FXML
+	private TableColumn<Cliente, Integer> tc_ID;
 
-    @FXML
-    private TableColumn<Cliente, String> tc_nome;
+	@FXML
+	private TableColumn<Cliente, String> tc_nome;
 
-    @FXML
-    private TableColumn<Cliente, String> tc_email;
+	@FXML
+	private TableColumn<Cliente, String> tc_email;
 
-    @FXML
-    private TableColumn<Cliente, String> tc_telefone;
+	@FXML
+	private TableColumn<Cliente, String> tc_telefone;
 
 //*********************** ON-ACTION *********************************
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		prepararTableView();
+		listar();
 	}
 
 	@FXML
@@ -70,23 +74,24 @@ public class TelaClienteController implements Initializable {
 	void OnClick_btn_incluir(ActionEvent event) {
 		utilJanela.novaJanelaComOwner("/br/view/TelaClienteIncluir.fxml", false);
 	}
-	
-	@FXML
-    void OnClick_btn_observacao(ActionEvent event) {
 
-    }
+	@FXML
+	void OnClick_btn_observacao(ActionEvent event) {
+		
+	}
 
 //************************** METODOS AUXILIARES *********************
 	private void listar() {
 		try {
-		
-
-			
+			ArrayList<Cliente> listaDeClientes;
+			ClienteDAO agenda = new ClienteDAO(nomeArquivo);
+			listaDeClientes = agenda.listar();
+			tableView_cliente.setItems(FXCollections.observableArrayList(listaDeClientes));
 		} catch (Exception erro) {
 			JOptionPane.showMessageDialog(null, erro.getMessage(), "ERRO", JOptionPane.ERROR_MESSAGE);
 		}
 	}
-	
+
 	private void prepararTableView() {
 		tc_ID.setCellValueFactory(new PropertyValueFactory<>("codCliente"));
 		tc_nome.setCellValueFactory(new PropertyValueFactory<>("nomeCliente"));
